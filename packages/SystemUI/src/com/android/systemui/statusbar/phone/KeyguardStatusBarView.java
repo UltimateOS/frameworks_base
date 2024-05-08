95ce1c65a23e7d9499114e01e6d5dd0aa822ab02
Pranav Vashi <neobuddy89@gmail.com>
2024-05-08 22:25:07 +0530

    SystemUI: Drop unused rounded corner padding
    * Wtf google?
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -102,7 +102,6 @@ public class KeyguardStatusBarView extends RelativeLayout {
     private int mCutoutSideNudge = 0;
 
     private DisplayCutout mDisplayCutout;
-    private int mRoundedCornerPadding = 0;
     // right and left padding applied to this view to account for cutouts and rounded corners
     private Insets mPadding = Insets.of(0, 0, 0, 0);
 
@@ -217,8 +216,6 @@ public class KeyguardStatusBarView extends RelativeLayout {
                 R.dimen.display_cutout_margin_consumption);
         mShowPercentAvailable = getContext().getResources().getBoolean(
                 com.android.internal.R.bool.config_battery_percentage_setting_available);
-        mRoundedCornerPadding = res.getDimensionPixelSize(
-                R.dimen.rounded_corner_content_padding);
     }
 
     private void updateVisibilities() {
